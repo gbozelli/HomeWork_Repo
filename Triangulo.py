e4c38cd0eefa651cce17a18c9d99e995eee2495b
Zobelli <bozelli222gabriel@gmail.com>
2023-01-06 11:58:48 -0300

    modified:   Triangulo.py 	modified:   test.py

--- Triangulo.py
+++ Triangulo.py
@@ -4,8 +4,8 @@ class Triangulo:
         self.a = a
         self.b = b
         self.c = c
-        list = [a,b,c]
-    def sort():
+        list = [self.a,self.b,self.c]
+    def sort(self, list):
         for i in range(1,2):
             key = list[i]
             j = i - 1
@@ -14,29 +14,31 @@ class Triangulo:
                 list[j+1] = list[j]
             list[j+1] = key
         return list
-    def test():
-        a = list[0]
-        b = list[1]
-        c = list[2]
-        if(c>a+b):
+    def test(self, list):
+        self. self.a = list[0]
+        self.self.b = list[1]
+        self.self.c = list[2]
+        if(self.c>self.a+self.b):
             print("É triângulo")
-        if((c)**2==(a)**2+(b)**2):
+        if((self.c)**2==(self.a)**2+(self.b)**2):
             print("É retângulo")
-        if(a==c):
+        if(self.a==self.c):
             print("É equilátero")
-        if(a==b or b==c and a!=c):
+        if(self.a==self.b or self.b==self.c and self.a!=self.c):
             print("É isósceles")
-        if(a!=b and b!=c):
+        if(self.a!=self.b and self.b!=self.c):
             print("É escaleno")
         else:
             print("Não é triângulo")
-    def perimeter(self,a,b,c):
-        p = a + b + c
+    def perimeter(self):
+        p = self.a + self.b + self.c
         return p
 
 T = Triangulo(2,3,4)
 T.a
 T.b
 T.c
+T.sort
 T.test
+T.perimeter
 T1 = Triangulo(4,6,8)
